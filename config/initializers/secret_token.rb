893531b66c97aed7182f3eea3dbf057cc932599d
Angad Singh <angad.singh@flipp.com>
2019-04-17 17:22:54 -0400

    rails new commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2992c83df711d44e457dde15772536d8f912f22de3fe203fa54f7dd0231aa0fee30a1abdc63d218085c816bd3864601707f4b1a469516579f05537c60dc2b9ae'
