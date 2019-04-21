2556b53fa833380f4e60fee53d71a611a5c5b1f2
Angad Singh <angad96@outlook.com>
2019-04-21 12:39:07 -0400

    Add show action to the controller and the view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,9 +18,6 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
-    def edit
-    end
-    
     private
     def article_params
         params.require(:article).permit(:title, :description)
